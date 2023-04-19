ee9967ed8e9388c60116e912100c514f84c2593a
GioGio4 <g.ceccobelli@outlook.it>
2023-04-19 15:42:00 +0200

    Create foreign key table Projects and seeder Type

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
+use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,8 +14,17 @@ class TypeSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $types_name = ['Games', 'Marketplace App', 'Web App', 'Mobile App', 'Authentication App'];
+
+        foreach ($types_name as $type_name) {
+            $type = new Type();
+
+            $type->name = $type_name;
+            $type->color = $faker->hexColor();
+
+            $type->save();
+        }
     }
 }
